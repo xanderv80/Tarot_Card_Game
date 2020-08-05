ac65e4111afdca802787e052a3e622d59fd66f05
Xander Villasenor <xanderslaboratory@gmail.com>
2020-08-05 15:19:06 -0600

    Canvas setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import "./styles.css";
+
+let canvas = document.getElementById("gameScreen");
+
+let ctx = canvas.getContext("2d");
+
+ctx.clearRect(0, 0, 800, 600);
+
+ctx.fillStyle = "#f00";
+//ctx.fillRect(20, 20, 100, 100);
+
+//ctx.fillStyle = '#00f';
+ctx.fillRect(20, 20, 100, 100);
