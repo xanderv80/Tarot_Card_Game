8e72384689ee5ea5b9ad7bb37f66c520bbeb096a
Xander Villasenor <xanderslaboratory@gmail.com>
2020-08-06 06:16:55 -0600

    import card.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import "./styles.css";
+import Card from "./card";
 
 let canvas = document.getElementById("gameScreen");
-
 let ctx = canvas.getContext("2d");
+
+const GAME_WIDTH = 800;
+const GAME_HEIGHT = 600;
 
 ctx.clearRect(0, 0, 800, 600);
 
-ctx.fillStyle = "#f00";
-//ctx.fillRect(20, 20, 100, 100);
+let card = new Card(GAME_WIDTH, GAME_HEIGHT);
 
-//ctx.fillStyle = '#00f';
-ctx.fillRect(20, 20, 100, 100);
+card.draw(ctx);
